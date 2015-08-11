fabed0cd8eb8503109c7897632a7892587b26680
umpirsky <umpirsky@gmail.com>
2015-08-11 16:03:35 +0200

    Initial commit of a micro-symfony version

---
+++ AppBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return $this->render('default/homepage.twig');
+    }
+}
